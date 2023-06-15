2fac63f6d50080f3c55019de9dd9901e0e62e9ec
Henrique Trajano <henrique.trajano.a@gmail.com>
2023-06-15 20:36:34 -0300

    changed spring initial method run to import static

--- src/main/java/br/com/trajy/graphql/GraphqlApplication.java
+++ src/main/java/br/com/trajy/graphql/GraphqlApplication.java
@@ -1,13 +1,14 @@
 package br.com.trajy.graphql;
 
-import org.springframework.boot.SpringApplication;
+import static org.springframework.boot.SpringApplication.run;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GraphqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraphqlApplication.class, args);
+		run(GraphqlApplication.class, args);
 	}
 
 }
