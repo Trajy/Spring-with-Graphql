e28e16296ef3dc89b71dfc0a6e8d1d373e860da1
Henrique Trajano <henrique.trajano.a@gmail.com>
2023-06-18 23:12:44 -0300

    fix Resolver Annotation
    Rewsolver works only with @Controller or @RestController annotations

--- src/main/java/br/com/trajy/graphql/resolver/ClienteResolver.java
+++ src/main/java/br/com/trajy/graphql/resolver/ClienteResolver.java
@@ -3,9 +3,9 @@ package br.com.trajy.graphql.resolver;
 import br.com.trajy.graphql.controller.ClienteController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
-@Component
+@Controller
 @RequiredArgsConstructor
 public class ClienteResolver {
 
