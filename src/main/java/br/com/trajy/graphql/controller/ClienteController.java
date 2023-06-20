12196a6b1b9270ed8b54bd7679b6217d01927a71
Henrique Trajano <henrique.trajano.a@gmail.com>
2023-06-20 07:44:30 -0300

    create crud methods into ClienteController

--- src/main/java/br/com/trajy/graphql/controller/ClienteController.java
+++ src/main/java/br/com/trajy/graphql/controller/ClienteController.java
@@ -1,6 +1,11 @@
 package br.com.trajy.graphql.controller;
 
-import br.com.trajy.graphql.model.Cliente;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static java.util.Objects.nonNull;
+
+import br.com.trajy.graphql.model.input.ClienteInput;
+import br.com.trajy.graphql.model.output.Cliente;
 import br.com.trajy.graphql.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -10,13 +15,33 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequiredArgsConstructor
 @SchemaMapping(typeName = "ClienteController")
-public class ClienteController extends Generalization {
+public class ClienteController {
 
     private final ClienteRepository repository;
 
     @SchemaMapping
-    public Cliente getCliente(@Argument Long id) {
-        return repository.findById(id).orElse(null);
+    public Cliente findById(@Argument Long id) {
+        return this.repository.findById(id).orElse(null);
+    }
+
+    @SchemaMapping
+    public Long save(@Argument ClienteInput cliente) {
+        return this.repository.save(cliente).getId();
+    }
+
+    @SchemaMapping
+    public Cliente update(@Argument ClienteInput cliente) {
+        return this.repository.save(cliente);
+    }
+
+    @SchemaMapping
+    public Boolean delete(@Argument Long id) {
+        Cliente cliente = this.findById(id);
+        if(nonNull(cliente)) {
+            this.repository.delete(cliente);
+            return TRUE;
+        }
+        return FALSE;
     }
 
 }
