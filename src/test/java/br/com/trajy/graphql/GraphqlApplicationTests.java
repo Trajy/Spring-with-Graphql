cc1212ddb420fe525eb039ee0f761cfe781affaa
Henrique Trajano <henrique.trajano.a@gmail.com>
2023-07-02 21:03:55 -0300

    improved unitTests and applied ObjectMother designPattern to tests

--- src/test/java/br/com/trajy/graphql/GraphqlApplicationTests.java
+++ src/test/java/br/com/trajy/graphql/GraphqlApplicationTests.java
@@ -1,14 +1,17 @@
 package br.com.trajy.graphql;
 
-import static com.github.javafaker.Faker.instance;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
 
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
+
+import br.com.trajy.graphql.objectmother.ClienteMother;
 import br.com.trajy.graphql.types.Cliente;
 import com.netflix.graphql.dgs.DgsQueryExecutor;
-import lombok.RequiredArgsConstructor;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.LinkedHashMap;
 
 @SpringBootTest
 class GraphqlApplicationTests {
@@ -17,24 +20,24 @@ class GraphqlApplicationTests {
 	private DgsQueryExecutor queryExecutor;
 
 	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	public void graphQlTest() {
-		String clienteNome = instance().name().name();
+	void saveGraphQLTest() {
 		String saveGraphQlQuery = "{" +
 				"clienteController {" +
 				"save(cliente: {" +
-				"nome: \"" + clienteNome + "\"" +
-				"documento: \"1234444\"" +
+				"nome: \"" + ClienteMother.CLIENTE.getNome() + "\"" +
+				"documento: \"" + ClienteMother.CLIENTE.getDocumento() +"\"" +
 				"pedidos: [{" +
 				"codigo: \"1234\"" +
 				"}]})}}";
-		String id = queryExecutor.executeAndExtractJsonPathAsObject(saveGraphQlQuery, "data.clienteController.save", String.class);
-		String findGraphQlQuery = "{" +
+		ClienteMother.id = queryExecutor.executeAndExtractJsonPathAsObject(saveGraphQlQuery, "data.clienteController.save", String.class);
+		assertNotNull("id nao retornado ao salvar", ClienteMother.id);
+	}
+
+	@Test
+	void findByIdGraphQLTest() {
+		final String findGraphQlQuery = "{" +
 				"clienteController {" +
-				"findById(id: \"" + id +"\") {" +
+				"findById(id: \"" + ClienteMother.id +"\") {" +
 				"nome\n" +
 				"documento\n" +
 				"pedidos {" +
@@ -42,8 +45,16 @@ class GraphqlApplicationTests {
 				"codigo" +
 				"}}}}";
 		Cliente cliente = queryExecutor.executeAndExtractJsonPathAsObject(findGraphQlQuery, "data.clienteController.findById", Cliente.class);
-		assertEquals("O nome nao foi salvo corretamente", clienteNome, cliente.getNome());
+		assertEquals("O nome nao foi salvo corretamente", ClienteMother.CLIENTE.getNome(), cliente.getNome());
+
 	}
+
+	@Test
+	void findGraphQLTest() {
+		final String filter = "{nome: \"" + ClienteMother.FILTER.getNome() + "\"}";
+		final String query = "{ clienteController { find(filter:" + filter + ") { nome }}}";
+		JSONArray clientes = queryExecutor.executeAndExtractJsonPath(query,"data.clienteController.find[*]");
+		clientes.forEach(cliente -> assertEquals("find nao retorna elementos corretos", ClienteMother.FILTER.getNome(), LinkedHashMap.class.cast(cliente).get("nome")));
+  }
+
 }
-
-
