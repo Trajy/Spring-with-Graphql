2a14491adbc3586fccb4ecff7176b1e2defde4c4
Henrique Trajano <henrique.trajano.a@gmail.com>
2023-07-07 07:35:41 -0300

    implemented method to catch request headers
    Dgs framework supports spring web @ResquestHeader and @RequestParam annotations, in this exemple the values is captured and just returned throw headersTest declared in graphQL schema

--- src/main/java/br/com/trajy/graphql/controller/HelloWorldController.java
+++ src/main/java/br/com/trajy/graphql/controller/HelloWorldController.java
@@ -2,6 +2,8 @@ package br.com.trajy.graphql.controller;
 
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @DgsComponent
 public class HelloWorldController {
@@ -9,5 +11,18 @@ public class HelloWorldController {
     @DgsQuery
     public String helloWorld() {
         return "hello GraphQl";
+    }
+
+    @DgsQuery
+    public String headersTest(
+            @RequestHeader(required = false) String optionalHeader,
+            @RequestHeader String mandatoryHeader,
+            @RequestParam(required = false) String optionalParam,
+            @RequestParam String mandatoryParam
+    ) {
+        return "{ \"optionalHeader\": \"" + optionalHeader + "\", "
+                + "\"mandatoryHeader\": \"" + mandatoryHeader + "\", "
+                + "\"optionalParam\": \"" + optionalParam + "\", "
+                + "\"mandatoryParam\": \"" + mandatoryParam + "\" }";
     }
 }
