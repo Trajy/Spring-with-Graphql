ec6eb211c370fd93b1c86d3cc285499f1e0fae77
Henrique Trajano <henrique.trajano.a@gmail.com>
2023-12-22 21:00:44 -0300

    added method to convert UserTokenEntity into UserAuthToken graphQl model data

--- src/main/java/br/com/trajy/graphql/assembly/UserAssembly.java
+++ src/main/java/br/com/trajy/graphql/assembly/UserAssembly.java
@@ -1,7 +1,9 @@
 package br.com.trajy.graphql.assembly;
 
 import br.com.trajy.graphql.codegen.tad.User;
+import br.com.trajy.graphql.codegen.tad.UserAuthToken;
 import br.com.trajy.graphql.entity.UserEntity;
+import br.com.trajy.graphql.entity.UserTokenEntity;
 import br.com.trajy.graphql.util.CommonUtil;
 import lombok.experimental.ExtensionMethod;
 import lombok.experimental.UtilityClass;
@@ -24,6 +26,13 @@ public class UserAssembly {
                 .setProblemz(entity.getProblemz().problemzToGraphQlModel())
                 .setSolutionz(entity.getSolutionz().solutionzToGraphQlModel())
                 .setCreatedAt(entity.getCreationTimestamp())
+                .build();
+    }
+
+    public UserAuthToken userTokenToGraphQlModel(UserTokenEntity entity) {
+        return UserAuthToken.builder()
+                .setToken(entity.getAuthToken())
+                .setExpiryTime(entity.getExpiryTimestamp())
                 .build();
     }
 
