ed47a18ed556310554b6450ff9b0c18ecaad9167
Henrique Trajano <henrique.trajano.a@gmail.com>
2024-03-06 09:45:30 -0300

    created service layer for problems

---
+++ src/main/java/br/com/trajy/graphql/service/ProblemService.java
@@ -0,0 +1,24 @@
+package br.com.trajy.graphql.service;
+
+import br.com.trajy.graphql.entity.ProblemEntity;
+import br.com.trajy.graphql.repository.ProblemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProblemService {
+
+    private final ProblemRepository repository;
+
+    public List<ProblemEntity> findLatest() {
+        return repository.findAllByOrderByCreationTimestampDesc();
+    }
+
+    public ProblemEntity findById(UUID id) {
+        return repository.findById(id).orElse(null);
+    }
+
+}
