b2b8a9fca41dbde0ed4e480f6a470a39ff2c575d
Henrique Trajano <henrique.trajano.a@gmail.com>
2024-03-06 09:41:55 -0300

    added method query by name for get all Problems ordered by creation date in Repository layer

--- src/main/java/br/com/trajy/graphql/repository/ProblemRepository.java
+++ src/main/java/br/com/trajy/graphql/repository/ProblemRepository.java
@@ -3,9 +3,12 @@ package br.com.trajy.graphql.repository;
 import br.com.trajy.graphql.entity.ProblemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProblemRepository extends JpaRepository<ProblemEntity, UUID> {
+
+    List<ProblemEntity> findAllByOrderByCreationTimestampDesc();
 
 }
