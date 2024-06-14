3889affe38c38da77af30ab9bd43b16cb8283e14
Henrique Trajano <henrique.trajano.a@gmail.com>
2024-06-14 16:25:21 -0300

    created exemple data seed for database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+   INSERT INTO userz VALUES
+   (1, TRUE, NULL, CURRENT_TIMESTAMP, 'Fulano de Tal', 'filano@gmail.com', '129837jhaljkhaskjhd27', 'Fulanoxz');
+
+   INSERT INTO problemz VALUES
+   (1, 'TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1),
+   (2, 'TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1),
+   (3, 'TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1),
+   (4, 'TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1);
