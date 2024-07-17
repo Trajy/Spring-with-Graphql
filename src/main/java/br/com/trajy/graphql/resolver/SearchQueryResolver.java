771de9ba99e7d7cfac174f72005aad6b0c25a5d2
Henrique Trajano <henrique.trajano.a@gmail.com>
2024-07-17 13:04:06 -0300

    changed parameter name in search query to "input"

--- src/main/java/br/com/trajy/graphql/resolver/SearchQueryResolver.java
+++ src/main/java/br/com/trajy/graphql/resolver/SearchQueryResolver.java
@@ -8,8 +8,10 @@ import br.com.trajy.graphql.codegen.tad.SearchInput;
 import br.com.trajy.graphql.codegen.tad.Searchable;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.exceptions.DgsEntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.ExtensionMethod;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,11 +25,11 @@ public class SearchQueryResolver {
 
     private final QueryResolver queryResolver;
 
-    @DgsQuery(field = "search")
-    public List<Searchable> search(SearchInput filter) {
+    @DgsQuery
+    public List<Searchable> search(SearchInput input) {
         List<Searchable> results = new ArrayList<>();
-        results.addAll(queryResolver.getProblemDomainResolver().findByKeyword(filter.getKeyword()));
-        results.addAll(queryResolver.getSolutionDomainResolver().findByKeyword(filter.getKeyword()));
+        results.addAll(queryResolver.getProblemDomainResolver().findByKeyword(input.getKeyword()));
+        results.addAll(queryResolver.getSolutionDomainResolver().findByKeyword(input.getKeyword()));
         results.sort(comparing(Searchable::getCreatedAt));
         return results;
     }
