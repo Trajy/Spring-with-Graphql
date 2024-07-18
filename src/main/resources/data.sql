d80ee355d9e68bb5a217e2294faa78ad546751e7
Henrique Trajano <henrique.trajano.a@gmail.com>
2024-07-17 21:33:38 -0300

    remove ids from database seed query file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
    INSERT INTO userz VALUES
    (1, TRUE, NULL, CURRENT_TIMESTAMP, 'Fulano de Tal', 'fulano', '$2y$05$OVLxYU3fbFTwWVPtY0TsauKZYu.It7O9CITEdcQG/Dbk1DmzpidCq', 'fulanoxz');
 
-   INSERT INTO problemz VALUES
-   (1, 'TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1),
-   (2, 'TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1),
-   (3, 'TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1),
-   (4, 'TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1);
+   INSERT INTO problemz(content, creation_timestamp, tags, title, user_id) VALUES
+   ('TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1),
+   ('TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1),
+   ('TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1),
+   ('TESTE', CURRENT_TIMESTAMP, 'tag_1, tag_2', 'Title_1', 1);
